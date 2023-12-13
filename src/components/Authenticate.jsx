21c9eaae1d81c362c87985f60951c597f32e500a
Samantha Garabedian <samanthagarabedian@samanthsonalair.lan>
2023-12-13 12:19:30 -0800

    even more updates

--- src/components/Authenticate.jsx
+++ src/components/Authenticate.jsx
@@ -1,12 +1,36 @@
+import { useState } from 'react'
+
 export default function Authenticate({ token }) {
-    async function handleClick () {
-      console.log("Clicked")
+    // State variables
+    const [successMessage, setSuccessMessage] = useState(null);
+    const [error, setError] = useState(null)
+
+    // Click function
+    async function handleClick() {
+        console.log("Clicked")
+        try {
+            const response = await fetch("https://fsa-jwt-practice.herokuapp.com/authenticate", {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${token}`,
+                },
+            }
+            );
+            const result = await response.json();
+            setSuccessMessage(result.message);
+
+        } catch (error) {
+            setError(error.message)
+        }
     }
 
     return (
         <>
-        <h2>Authentication</h2>
-        <button onClick= {() => {handleClick()}} > Authenticate Token</button>
+            <h2>Authentication</h2>
+            {successMessage && <p>{successMessage}</p>}
+            {error && <p>{error}</p>}
+            <button onClick={() => { handleClick() }} > Authenticate Token</button>
         </>
     );
 }
